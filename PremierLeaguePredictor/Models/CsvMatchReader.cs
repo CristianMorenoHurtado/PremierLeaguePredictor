fb72d4dce8bb704b2bcb289bab9b0b60587df008
CristianMorenoHurtado <cmoreno@cleaverbrooks.com>
2025-02-18 09:58:24 -0500

    Added CsvMatchReader class.

---
+++ PremierLeaguePredictor/Models/CsvMatchReader.cs
@@ -0,0 +1,31 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+
+namespace PremierLeaguePredictor.Models
+{
+    /// <summary>
+    /// Class used for reading the match csv file.
+    /// </summary>
+    public class CsvMatchReader
+    {
+        /// <summary>
+        /// Method used for reading the match csv file.
+        /// </summary>
+        /// <returns>A list of MatchData objects.</returns>
+        public static List<MatchData> ReadCsvFile()
+        {
+            string filepath = $@"..\wwwroot\resources\results.csv";
+
+            using StreamReader reader = new(filepath);
+            using CsvReader csv = new(reader, new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true,  // First row is headers
+                MissingFieldFound = null // Ignore missing fields
+            });
+
+            IEnumerable<MatchData> records = csv.GetRecords<MatchData>();
+            return new List<MatchData>(records);
+        }
+    }
+}
